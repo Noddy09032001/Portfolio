c6fa3875ae8dd9882d481236456e273904ea0a48
Noddy09032001 <niranjan.dhokarikar2001@gmail.com>
2026-05-17 14:37:40 +0530

    Added code for dark mode configuration

---
+++ portfolio-new/app/providers.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import AOS from "aos";
+import { ThemeProvider } from "next-themes";
+import { useEffect } from "react";
+
+export default function Providers({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  useEffect(() => {
+    AOS.init({
+      duration: 800,
+      delay: 50,
+    });
+  }, []);
+
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem={false}
+    >
+      {children}
+    </ThemeProvider>
+  );
+}
