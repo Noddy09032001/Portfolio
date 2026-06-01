a826ab8c4cfbde475ec50839909067c74f3975ee
Noddy09032001 <niranjan.dhokarikar2001@gmail.com>
2026-06-01 10:55:06 +0530

    Added code for common components and elements
    1. Added code for common components Button, Card, Tooltip and Image components.
    
    2. Refactored common code and corrected indentation while removing unwanted code

---
+++ portfolio-new/src/common/components/elements/Image.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import clsx from 'clsx';
+import NextImage, { ImageProps as NextImageProps } from 'next/image';
+import { useState } from 'react';
+
+import clsxm from '../../libs/clsxm';
+
+type ImageProps = {
+  rounded?: string;
+} & NextImageProps;
+
+const Image = (props: ImageProps) => {
+  const { alt, src, className, rounded, priority, sizes, ...rest } = props;
+  const [isLoading, setLoading] = useState(true);
+
+  return (
+    <div
+      className={clsx(
+        'overflow-hidden',
+        isLoading ? 'animate-pulse' : '',
+        rounded,
+      )}
+    >
+      <NextImage
+        className={clsxm(
+          'duration-700 ease-in-out',
+          isLoading
+            ? 'scale-[1.02] blur-xl grayscale'
+            : 'scale-100 blur-0 grayscale-0',
+          rounded,
+          className,
+        )}
+        src={src}
+        alt={alt}
+        loading={priority ? undefined : 'lazy'}
+        priority={priority}
+        quality={85}
+        sizes={sizes}
+        onLoad={() => setLoading(false)}
+        {...rest}
+      />
+    </div>
+  );
+};
+export default Image;
