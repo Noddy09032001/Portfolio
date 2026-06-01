a0d64d71a1477cbdcee11c78b6a17a5706e8e57e
Noddy09032001 <niranjan.dhokarikar2001@gmail.com>
2026-06-01 11:00:45 +0530

    Added type definitions for constants

---
+++ portfolio-new/src/common/types/education.ts
@@ -0,0 +1,14 @@
+export interface EducationProps {
+  school: string;
+  major: string;
+  logo: string;
+  location: string;
+  degree: string;
+  start_year: number;
+  end_year: number;
+  link: string;
+  responsibilities?: {
+  coursework: string[];
+  achievements: string[];
+};
+}
